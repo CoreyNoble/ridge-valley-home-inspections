e116f3f887d465c12d9851a2e84aee8e462b9f85
Corey Noble <coreynoble@hotmail.ca>
2019-03-24 20:40:50 -0400

    Global - Tweaks / Adjustments before QA

--- src/pages/mail.php
+++ src/pages/mail.php
@@ -1,25 +1,60 @@
 <?php
-$to = "coreynoble@hotmail.ca"; // REPLACE!
-$subject = "Inquiry | Ridge Valley";
-$headers = "From: jruth@ridgevalley.ca"; // REPLACE?
+//Checking For reCAPTCHA
+$captcha;
+if (isset($_POST['g-recaptcha-response'])) {
+    $captcha = $_POST['g-recaptcha-response'];
+}
+// Checking For correct reCAPTCHA
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdKrJkUAAAAADRgsblMC4OFH-vJm4VE75gy0OC0&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+if (!$captcha || $response.success == false) {
+    echo "Your CAPTCHA response was wrong.";
+    exit ;
+} else {
+    // Checking For Blank Fields..
+    if ($name == "" || $email == "" || $address == "") {
+        echo "Please fill in all required* fields";
+    } else {
+        // Sanitize E-mail Address
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        // Validate E-mail Address
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if (!$email) {
+            echo "Invalid email address";
+        } else {
+            $to = "coreynoble@hotmail.ca"; // REPLACE!
+            $subject = "Inquiry | Ridge Valley";
+            $headers = 'From:' . $email . "\r\n";
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$address = $_POST['address'];
-$agent = $_POST['agent'];
-$details = $_POST['details'];
+            $name = $_POST['name'];
+            $phone = $_POST['phone'];
+            $address = $_POST['address'];
+            $agent = $_POST['agent'];
+            $details = $_POST['details'];
 
-$txt = "New Inquiry from Ridge Valley! \r\n\r\n"
-. "Contact Information: \r\n"
-. "Name: " . $name
-. "Email: " . $email
-. "Phone Number: " . $phone
-. "Address to be Inspected: " . $address
-. "Real estate agent: " . $agent
-. "\r\n\r\n"
-. "Inquiry Message: \r\n"
-. $details;
-
-mail($to,$subject,$txt,$headers);
-?> 
+            $message = "New Inquiry from Ridge Valley! \r\n\r\n"
+            . "Contact Information: \r\n"
+            . "Name: " . $name
+            . "Email: " . $email
+            . "Phone Number: " . $phone
+            . "Address to be Inspected: " . $address
+            . "Real estate agent: " . $agent
+            . "\r\n\r\n"
+            . "Inquiry Message: \r\n"
+            . $details;
+            
+            // Sender's Email
+            // Message lines should not exceed 70 characters (PHP rule), so wrap it
+            $message = wordwrap($message, 70, "\r\n");
+            // Send Mail By PHP Mail Function
+            if (mail($to, $subject, $message, $headers)) {
+                header("Location: /success.html"); /* Success */
+                exit();
+            } else {
+                header("Location: /failed.html"); /* Fail */
+                exit ;
+            }
+        }
+    }
+}
+?>
