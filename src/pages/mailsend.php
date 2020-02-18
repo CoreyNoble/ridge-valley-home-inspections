ddbae682e8184245946fc796da9445cb7976cca9
Corey Noble <coreynoble@hotmail.ca>
2020-02-18 18:38:20 -0500

    Kyle Young - Mailsend MIME Type

--- src/pages/mailsend.php
+++ src/pages/mailsend.php
@@ -1,6 +1,7 @@
 <?php
+
   $captcha;
-  
+
   if (isset($_POST['name'])) {
     $name=$_POST['name'];
   }
@@ -27,14 +28,19 @@
     header('Location:contact.html?captcha=none');
     exit;
   }
-
+REQUIRE_ONCE( "htmlMimeMail/htmlMimeMail5.php" );
+	$host	= 'tls://smtp.gmail.com';
+			$port	= 465;
+			$user	= '';
+			$pass	= '';
+	$from	='jruth@ridgevalley.ca';
   $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LdKrJkUAAAAADRgsblMC4OFH-vJm4VE75gy0OC0&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
 
   if ($response.success==false) {
     header('Location:contact.html?captcha=failed');
   } else {
     // MESSAGE PROPERTIES
-    $to = 'coreynoble@hotmail.ca';
+
     $subject = 'Inquiry - Ridge Valley Home Inspections';
 
     // USER DATA VARIABLES
@@ -57,9 +63,25 @@
     "\r\n". "\r\n". "\r\n";
 
     // SEND
-    mail($to, $subject, $message);
+
+		$mail = new htmlMimeMail5();
+		$mail->setHeader('X-Mailer', 'Ridge Valley (https://ridgevalley.ca)');
+						$mail->setSMTPParams( ((ISSET($host) AND TRIM($host)!='')?TRIM($host):ini_get("SMTP")),
+										      ((ISSET($port) AND TRIM($port)!='')?TRIM($port):ini_get("smtp_port")),
+										       null,
+										      ((ISSET($user) AND TRIM($user)!='' AND ISSET($pass) AND TRIM($pass)!='')?true:false),
+										      ((ISSET($user) AND TRIM($user)!='')?TRIM($user):null),
+										      ((ISSET($pass) AND TRIM($pass)!='')?TRIM($pass):null)
+										);
+						$mail->setReturnPath(((ISSET($from) AND TRIM($from)!='')?TRIM($from):ini_get("sendmail_from")));
+						$mail->setTextCharset('iso-8859-1');
+						$mail->setFrom($from);
+						$mail->setSubject($subject);
+				$mail->setText($message);
+				$mail->send(array('jruth@ridgevalley.ca'),'smtp');
+
 
     // REDIRECT TO CONFIRM
-    header('Location:maintenance.html');
+ header('Location:maintenance.html');
   }
 ?>
